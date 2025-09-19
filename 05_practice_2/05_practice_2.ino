b396f69adb164c57feace1bef64d9999f29bc491
korendia <ih.bae25@gmail.com>
2025-09-20 01:03:10 +0900

    Update 05_practice_2.ino

--- 05_practice_2/05_practice_2.ino
+++ 05_practice_2/05_practice_2.ino
@@ -5,16 +5,17 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); 
+  digitalWrite(LED_PIN, LOW);
   delay(1000);
-  
+
   for (int i = 0; i < 5; i++) {
-    digitalWrite(LED_PIN, HIGH); 
+    digitalWrite(LED_PIN, LOW);   // ON
     delay(100);
-    digitalWrite(LED_PIN, LOW);  
+    digitalWrite(LED_PIN, HIGH);  // OFF
     delay(100);
   }
-  digitalWrite(LED_PIN, LOW);
+
+  digitalWrite(LED_PIN, HIGH);    // OFF
   
   while (1) {}
 }
